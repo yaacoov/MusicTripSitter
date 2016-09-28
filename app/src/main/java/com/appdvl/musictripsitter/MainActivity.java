994bf08c4c6cc75d778982ef23033cb10a4d441a
yacov <Yaacoov@gmail.com>
2016-09-28 21:49:35 +0300

    starting the best music app

--- app/src/main/java/com/appdvl/musictripsitter/MainActivity.java
+++ app/src/main/java/com/appdvl/musictripsitter/MainActivity.java
@@ -5,9 +5,17 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        showMusicFragment();
+    }
+
+    private void showMusicFragment() {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.musicFragment, new MusicFragment())
+                .commit();
     }
 }
